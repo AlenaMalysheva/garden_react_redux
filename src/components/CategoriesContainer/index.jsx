5d4fa0d2f9a18a10fbb5396f60deb38856ad0609
malys@LAPTOP-DSQPC4U1 <alenamalyseva422@gmail.com>
2023-01-30 17:41:40 +0200

    fetch request + categoriesReducer + component categoriesContainer added

--- src/components/CategoriesContainer/index.jsx
+++ src/components/CategoriesContainer/index.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch,useSelector } from 'react-redux'
+import { getCategories } from '../../requests/categories_req'
+import CategoryCard from '../CategoryCard';
+import s from './index.module.css'
 
 export default function CategoriesContainer() {
+
+    const dispatch = useDispatch();
+
+    const categories = useSelector(state => state.categories);
+
+    useEffect(() => {
+        dispatch(getCategories)
+    }, [])
+   
+
   return (
-    <div>CategoriesContainer</div>
+    <section className={s.categories_block}>
+          <div className='wrapper'>
+            <div className={s.categories_info}>
+                <p className={s.categories_title}>Categories</p>
+                <button className={s.categories_btn}>All categories</button>
+            </div>
+            <div className={s.categories_container}>
+                {
+                    categories.map((el) => <CategoryCard key={el.id} {...el}/>).slice(0,4)
+                }
+            </div>
+        </div>
+    </section>
+  
   )
 }
--- src/components/CategoryCard/index.jsx
+++ src/components/CategoryCard/index.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import s from './index.module.css'
 
-export default function CategoryCard() {
+export default function CategoryCard({title, image}) {
   return (
-    <div>CategoryCard</div>
+    <div className={[s.category_card].join(' ')}>
+        <img src={`http://127.0.0.1:3333${image}`} alt="category" className={s.category_img} />
+        <p className={s.category_title}>{title}</p>
+    </div>
   )
 }
