5195ae43215d663a5d246ccde030e5b991a29cf4
malys@LAPTOP-DSQPC4U1 <alenamalyseva422@gmail.com>
2023-02-19 15:40:19 +0200

    productCard and search of products from min to max updated

--- src/components/ProdsCategoryContainer/index.jsx
+++ src/components/ProdsCategoryContainer/index.jsx
@@ -26,13 +26,37 @@ export default function ProdsCategoryContainer() {
         dispatch(sortProducts(event.target.value))
     } 
     
+    // поиск от min к max без применения button, возможно введение или min или max
+    const [ minPrice, setMinPrice ] = useState('');
+    const [ maxPrice, setMaxPrice ] = useState('');
+   
     const search_products = event => {
         event.preventDefault();
         const { min , max } = event.target;
         const min_value = min.value || 0;
         const max_value = max.value || Infinity;
-        dispatch(searchProducts({ min_value, max_value }));
+       
+        if (min_value !== '' && max_value !== ''){
+            dispatch(searchProducts({ min_value, max_value }));
+        }
+        if (min_value) {
+            dispatch(searchProducts({ min_value }));
+        } 
+        if (max_value) {
+            dispatch(searchProducts({ max_value }));
+        }
     }
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => { // для задержки,чтобы была возможность ввести оба значения
+          if (minPrice || maxPrice) {
+            dispatch(searchProducts({ min_value: minPrice || 0 , max_value: maxPrice || Infinity }))
+            }
+        }, 500);
+        return () => clearTimeout(timeoutId);
+      }, [ minPrice, maxPrice ]);
+
+    // фильтрация по наличию цены
 
     const [onSale, setOnSale] = useState(false);
 
@@ -43,21 +67,36 @@ export default function ProdsCategoryContainer() {
     return (
     <section className={s.products_section}>
         <div className='wrapper'>
-        <h2>{category.title}</h2>
+            <h2>{category.title}</h2>
             <div className={s.sorting}>
                 <div>
-                    <span className={s.form_title}>Price </span>
+                    <span className={s.form_title}>Price</span>
                     <form className={s.search_form} onSubmit={search_products}>
-                        <input type="number" placeholder='from' name='min'  min='0'/>
-                        <input type="number" placeholder='to' name='max'  min='0'/>
-                        <button>Search</button>
+                        <input 
+                            type="number" 
+                            placeholder='from' 
+                            name='min'  
+                            min='0' 
+                            value={minPrice} 
+                            onChange={(event) => setMinPrice(event.target.value)}
+                        />
+                        <input 
+                            type="number" 
+                            placeholder='to' 
+                            name='max'  
+                            min='0' 
+                            value={maxPrice} 
+                            onChange={(event) => setMaxPrice(event.target.value)}
+                        />
                     </form>
                 </div>
-
-                    <label htmlFor="discountCheckbox" className={s.label_discount}>Discounted items</label>
-                    <input type="checkbox" id="discountCheckbox" checked={onSale} onChange={() => setOnSale(!onSale)}
-                    />
-            
+                <label htmlFor="discountCheckbox" className={s.label_discount}>Discounted items</label>
+                <input 
+                        type="checkbox" 
+                        id="discountCheckbox" 
+                        checked={onSale} 
+                        onChange={() => setOnSale(!onSale)}
+                />
                 <div>
                     <span className={s.form_title}>Sorted </span>
                         <select className={s.sort_select} onInput={sort_product}>
@@ -67,16 +106,13 @@ export default function ProdsCategoryContainer() {
                         </select>
                 </div>
             </div>
-            
             <div className={s.products_container}>
-
-            {
-            prodsBycategory
+                {
+                    prodsBycategory
                     .filter((el) => !el.hide_elem)
                     .map((el) => (
-                    <ProductCard key={el.id} {...el} />
-            ))}
-                
+                        <ProductCard key={el.id} {...el} />))
+                }
             </div>
         </div>
     </section>
